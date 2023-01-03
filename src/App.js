d6cf142ba443b90139772cd7567eaee370c4eb79
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-03 18:32:37 +0530

    learned componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      data:null
+    }
+    console.warn("Constructor Called")
+  }
+  
+  componentDidMount(){
+    this.setState({data:"Ayaan"})
+    console.warn("componentDidMount")
+  }
+  render(){
+    console.warn("Render")
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+        </header>
+      </div>
+    );
+  
+  }
 }
 
 export default App;
