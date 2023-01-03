426a2e6556900f592263b67ef64ab1ed9c63093b
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-03 19:43:57 +0530

    Learned ComponentDidUpdate

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ export class App extends React.Component {
     }
   }
 
+  // componentDidUpdate is called when the state of any component is updated or changed 
   componentDidUpdate() {
     console.warn("ComponentDidUpdate")
+    // here if you don't put the loop then it will go inside the infinite loop 
     if( this.state.who == null ){
+      // becasue everytime state is changed the update method will called and inside the update method we are again changing the state this will form a loop
       this.setState({
         active:"The",
         who:"Ayaan Shaikh"
@@ -25,6 +28,7 @@ export class App extends React.Component {
     return (
       <div className="App">
         <h1>Hello from {this.state.active} {this.state.who}</h1>
+        {/* here when this state will be changed first time then the componentDidUpdate will be called  */}
         <button onClick={() => {
           this.setState({
             active:"The",
