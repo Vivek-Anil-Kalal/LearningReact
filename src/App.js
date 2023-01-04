78abfce088446c302b4576b01cb517e26fb8aa85
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-04 19:31:33 +0530

    all comments done

--- src/App.js
+++ src/App.js
@@ -3,28 +3,12 @@ import './App.css';
 import React from 'react';
 
 export class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      data:null
-    }
-    console.warn("Constructor Called")
-  }
 
-  // this method will be called when the component will be fully loaded and also after the render method is called 
-  // u can see in the output in console there the construtor called first then the render method is called then this method is called
-  // after the state is changed the render method is re called ...
-  componentDidMount(){
-    this.setState({data:"Ayaan"})
-    console.warn("componentDidMount")
-  }
   render(){
-    console.warn("Render")
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-  
+        <h1>Everthing is in other branches Checkout the total branches</h1>        
         </header>
       </div>
     );
