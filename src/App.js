a6c4c782a2254a01610001d914f909c2e14a4000
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-05 17:55:55 +0530

    Learned Map function

--- src/App.js
+++ src/App.js
@@ -6,57 +6,49 @@ export class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      email: "",
-      password: "",
-      emailError: "",
-      passwordError: ""
+      list: [
+        { name: "Ayaan Shaikh", email: "ayaan@gmail.com", phoneNumber: "7984564991" },
+        { name: "Vv", email: "a@gmail.com", phoneNumber: "9999999999" },
+        { name: "Dabba", email: "vv@gmail.com", phoneNumber: "9866456433" },
+        { name: "Dabba", email: "vv@gmail.com", phoneNumber: "9866456433" },
+      ]
     }
   }
 
-  isValid() {
-    if (!this.state.email.includes("@")) {
-      console.log(this.state.email.value)
-      this.setState({
-        emailError: "Invalid Email"
-      })
-    } else if (this.state.password.length < 5) {
-      this.setState({
-        passwordError: "Invalid ! Password lenght must be greater or equal to 5"
-      })
-    } else {
-      return true;
-    }
-
-  }
-  submit() {
-    // this binding with this is necessary nhi toh vo isValid method ko pehchane ga hi nhi
-    if (this.isValid()) {
-      this.setState({
-        emailError: "",
-        passwordError: ""
-      })
-      alert("Form is Submitted")
-    }
-  }
 
   render() {
     return (
       <div className="App">
-        <h1>Form Validation</h1>
-        Email : <input type="text" onChange={(event) => {
-          console.log(this.state.email)
-          this.setState({
-            email: event.target.value
-          })
-        }} />
-        <p>{this.state.emailError}</p>
-        Password : <input type="password" onChange={(event) => {
-          this.setState({
-            password: event.target.value
-          })
-        }} />
-        <p>{this.state.passwordError}</p>
-        <button onClick={() => this.submit()}>Submit</button>
+        <h1>Listing the Elements</h1>
+        <table cellSpacing="10" align='center' border="2" >
+          <thead>
+            <th>Name </th>
+            <th>Email </th>
+            <th>PhoneNumber </th>
+          </thead>
+          <tbody>
+            {/* <tr>
+              <td>{this.state.list.map((item) =>
+                <div>{item.name}</div>
+              )}</td>
+              <td>{this.state.list.map((item) =>
+                <div>{item.email}</div>
+              )}</td>
+              <td>{this.state.list.map((item) =>
+                <div>{item.phoneNumber}</div>
+              )}</td>
+            </tr> */}
+            {
+              this.state.list.map((item)=>
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phoneNumber}</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
       </div>
     );
 
