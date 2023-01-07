ee757b9681353f7309b994b7011e1d20caf8917f
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-07 17:27:58 +0530

    Learned Rounting and Solved Bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Home from './cmp/Home';
 import About from './cmp/About';
 import Forms from './cmp/Forms';
-import { BrowserRouter as Router, Route, Link, Routes, Form } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
 export class App extends React.Component {
 
@@ -11,18 +11,14 @@ export class App extends React.Component {
     return (
       <div className="App">
         <Router>
-          {/* <Home />
-          <About />
-          <Forms /> */}
-          {/* These are the url-pattern kind off */}
-          <Link to="" >Home </Link>
-          <Link to="/about" > About </Link>
-          <Link to="/forms" > Forms </Link>
+          <Link to="">Home </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/forms"> Forms </Link>
 
           <Routes>
-            <Route exact path="/" element={Home} />
-            <Route path="/about" element={About} />
-            <Route path="/forms" element={Forms} />
+            <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/forms' element={<Forms/>} />
           </Routes>
         </Router>
       </div>
--- src/cmp/About.js
+++ src/cmp/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function About(){
     return <div>
+        {console.log("Hello ")}
         <h1>This is About Component</h1>
     </div>
 }
