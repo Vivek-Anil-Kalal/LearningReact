8f4e96c16377be166c9173ddc95f461c1feaa4ed
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-08 11:10:09 +0530

    Learned Ref

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import './App.css';
 import React from 'react';
-import Other from './cmp/Other';
+
+/* Ref is used to manipulates the dom we are recommended not to directly manipulate DOM though if in case we need to we can 
+edit or change it with the help of Ref */ 
 
 export class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      users: null,
-    }
+    this.userRef=React.createRef();
   }
 
   sty = {
     color: "blue",
     fontSize: "56px"
   }
-
+  // this function is to gain the focus on the text field when the button is clicked
+  editValue(){
+    // using userRef we can get focus on element 
+    this.userRef.current.focus() ;
+    // using userRef we can Change the value of element
+    this.userRef.current.value = "Ayaan Shaikh" ;
+  }
 
   render() {
     return (
       <div className="App">
-        <h1 style={this.sty}>Learning Portal Creation</h1>
-        <h1 style={this.sty}>Check Debugger Tool</h1>
-        <Other />
+        <h1 style={this.sty}>Showing the use of Ref in React</h1>
+        {/* for the Element for which we want to change in dom we can give it attribute ref to use it */}
+        <input ref={this.userRef} type="text" />
+        <button onClick={ ()=> this.editValue()}>Click Me to Gain focus</button>
       </div>
     );
 
